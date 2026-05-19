06e7c4ed88a31a240e0293154c25dcac5e21dac1
SB-2803 <siyabaheti2803@gmail.com>
2026-05-19 14:04:07 +0530

    Birthday homework

---
+++ Module 1/Storing birthday dates.py
@@ -0,0 +1,20 @@
+#Activity 4
+#Creating 5 different variables
+print("Storing birth dates of 5 of my friends! \n")
+x ="28th March 2013"
+y ="12th September 2011"
+z ="5th September 2016"
+a ="1st July 2011"
+b ="14th November 2012"
+print("Sonakshi's birthday is on",y)
+print("Kashvi's birthday is on",a)
+print("Siya's birthday is on",x)
+print("Raavi's birthday is on",z)
+print("Tanvi's birthday is on",b )
+
+#User input
+name= input("enter your name : ")
+birthday= input("enter your birthday : ")
+print("\nThank you", name, "for sharing you birthday on", birthday, "with us!")
+
+
