feb34109e533ce5bb00297b28f6870f0da52d2d8
SB-2803 <siyabaheti2803@gmail.com>
2026-05-19 13:39:10 +0530

    First class

---
+++ Module 1/Prints.py
@@ -0,0 +1,15 @@
+#Activity 1
+#Print a Sentence
+print("Welcome to the world of programming!")
+
+#Print a Number
+print(28)
+
+#Next Line after Print
+print("Hello Python \n")
+
+#Pass multiple arguements for printing
+print("hello" ,5)
+
+#End arguement of Print Statement
+print("Welcome to" , end="#")
