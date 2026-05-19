780a4c1f6360aff650b94ffa5849cc62372ab3cd
SB-2803 <siyabaheti2803@gmail.com>
2026-05-19 13:33:34 +0530

    First commit

---
+++ Module 1/Keywords.py
@@ -0,0 +1,6 @@
+#Activity 3
+import keyword
+
+# print all the python keywords
+print("Python keywords are... \n")
+print(keyword.kwlist)
---
+++ Module 1/Variabes.py
@@ -0,0 +1,14 @@
+#Activity 2
+#Creating different variables
+x = 5
+y = "John"
+print("x=",x)
+print("y=",y)
+
+codingal = "codingal"
+print("codingal")
+
+#User Input
+name = input("enter your name : ")
+
+print("\nHello", name, "\nWelcome to Codingal")
