7ce6a78b8e1e468e4d668795d183d040b766295b
SB-2803 <siyabaheti2803@gmail.com>
2026-05-22 13:38:26 +0530

    Data types

---
+++ String Reverse.py
@@ -0,0 +1,10 @@
+#input a aword
+text = str(input("Enter a String : "))
+
+# Reverse String
+# using step value as -1 to iterate in reverse
+# var[start:stop:step]
+revText = text[::-1]
+
+print("Reverse of Given String is: ")
+print(revText)
---
+++ Typecasting.py
@@ -0,0 +1,32 @@
+# Assigning Different Variables
+name= "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+# Printing Different Variables and their Datatypes
+print("Name :", name)
+print("Data Type of Name is :", type(name))
+
+print("Age :", age)
+print("Data Type of Age is :", type(age))
+
+print("Weight :", weight)
+print("Data Type of Weight is :", type(weight))
+
+print("Is_student :", is_student)
+print("Data Type of Name is :", type(is_student))
+
+# Type casting to convert the datatype of variables
+print("\n After Type casting...")
+age = str(age)
+print(age)
+print("Data Type of age is :", type(age))
+
+weight = int(weight)
+print(weight)
+print("Data Type of weight is :", type(weight))
+
+age = float(age)
+print(age)
+print("Data Type of Age is :", type(age))
