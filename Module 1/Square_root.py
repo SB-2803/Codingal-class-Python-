8dae172a4a966ed281f4f4bc4bc08941bc60cd8f
SB-2803 <siyabaheti2803@gmail.com>
2026-05-26 14:08:03 +0530

    Math operations.3

---
+++ Module 1/Square_root.py
@@ -0,0 +1,14 @@
+print("Today we will calculate the Square Root of ANY NUMBER using the formula = Number*0.5. But first let us see 2 examples!")
+print("Example 1 (of a square number) -")
+num = 49
+sqr = (num**0.5)
+print("Square Root of 49 :")
+print(sqr)
+print("Example 2 (of a non-square number) -")
+numb = 99
+sqro = (numb**0.5)
+print("Square Root of 99 :")
+print(sqro)
+number = int(input("Enter Any Number of your choice to find the Square Root of that Number ="))
+sr = (number ** 0.5)
+print("Square Root of",number,"is :",sr)
