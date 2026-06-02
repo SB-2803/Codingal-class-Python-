4f37f3e71d8b4e587f41ac23828e78210e47b562
SB-2803 <siyabaheti2803@gmail.com>
2026-06-02 13:29:22 +0530

    Logical operators and conditional statements

--- Module 1/Weather.py
+++ Module 1/Weather.py
@@ -4,8 +4,8 @@ sex=input("Enter your gender:")
 home=input("Enter the place where you live:")
 climate=input("Enter the climate of the place where you live:")
 month=input("Enter the month right now:")
-weather=int(input("Enter whether it is hot or cold, in case of hot enter 1 and in case of cold enter 2: "))
-if (weather == 1):
+weather=input("Enter whether it is hot or cold: ")
+if (weather == "hot"):
     print("You can wear shorts with a t-shirt. You are advised to wear cotton and light-coloured clothes.")
     print("If you are a female you can even wear a frock.")
 else:
