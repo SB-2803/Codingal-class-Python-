76a7173c17573608e63b593a1b1a5e516c8531dc
SB-2803 <siyabaheti2803@gmail.com>
2026-06-07 14:09:22 +0530

    Fire brigade

--- Module 1/Checking whether to call fire brigade.py
+++ Module 1/Checking whether to call fire brigade.py
@@ -1,5 +1,12 @@
 name = input("Enter your name:")
 print("Today we will check whether you should call the fire brigade or not. To do please fill the following:")
-fire = input("Enter yes if you can see and fire or bright light around you:")
-smoke = input("Enter yes if you can see or smell and kind of smoke around you:")
-place = input("E     ")
+fire = input("Enter yes if you can see fire flames:")
+cooking = input("Enter yes if there is flame cooking going on in your house:")
+if fire == "yes" and cooking == "yes":
+    print(name,"you don't need to call a fire brigade. YOU ARE SAFE!!!!")
+elif fire == "yes" and cooking == "no":
+    print(name,"immediately call a fire brigade. YOU ARE IN DANGER!!!")
+elif fire == "no" and cooking == "no":
+   print(name,"you don't need to call a fire brigade. YOU ARE SAFE!!!!")
+elif fire == "no" and cooking == "yes":
+   print(name,"you don't need to call a fire brigade. YOU ARE SAFE!!!!")
