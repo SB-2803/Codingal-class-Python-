b16897e85eb41b617c64ce60d4757e85e817d846
SB-2803 <siyabaheti2803@gmail.com>
2026-06-09 17:12:28 +0530

    number swap

---
+++ Module 1/Number swap.py
@@ -0,0 +1,21 @@
+print("The wrong values of a, b and c are:")
+a = 9
+b = 2
+c = 3
+print("a = 9")
+print("b = 2")
+print("c = 3")
+print("The equation is a*b+c.")
+print("The wrong answer is 21")
+wrong_answer = 21
+print("The correct values of a,b and c are:")
+print("a = 3")
+print("b = 9")
+print("c = 2")
+a = 3
+b = 9
+c = 2
+correct_answer = a*b+c # type: ignore
+print("The correct answer is =",correct_answer)
+
+
