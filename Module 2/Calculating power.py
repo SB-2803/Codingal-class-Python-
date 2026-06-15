f7baa368bb673ca7823e6a5ec894aa2cd3bd0114
SB-2803 <siyabaheti2803@gmail.com>
2026-06-15 19:14:02 +0530

    power

---
+++ Module 2/Calculating power.py
@@ -0,0 +1,5 @@
+base = int(input("Enter the number which is the base:"))
+power = int(input("Enter the number which is the power:"))
+for i in range(1,power+1):
+    result = i*base**power
+    print("your answer is",result)
