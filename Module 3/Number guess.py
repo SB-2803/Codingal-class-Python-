a8dfd1780b9a8c41b327e4eab190337b30984e47
SB-2803 <siyabaheti2803@gmail.com>
2026-07-24 16:53:17 +0530

    gueesw

--- Module 3/Number guess.py
+++ Module 3/Number guess.py
@@ -4,16 +4,16 @@ num = str(random.randint(10,20))
 
 print("A number between 10 to 20 will be generated and you have to guess the number in 5 chances.")
 print("The game ends when you get the correct guess!!")
-i = 6
+i = 5
   
-while playing:
-    guess = input("Give me your best guess!! \n")
-    for i in range(1,6):
-         i = i-1
-         if num == guess:
-             print("You win the game!!")
-             print("The number was",num)
-             break
-         else:
-              print("You guess isn't quite right. Try agin!!")
-              print("You have",i, "no. of chances left")
+while i>0:
+    guess = input("Give me your best guess!! \n")  
+    i = i-1      
+    if num == guess:
+        print("You win the game!!")
+        print("The number was",num)
+        break
+    else:
+        print("You guess isn't quite right. Try agin!!")
+        print("You have",i, "no. of chances left")
+    
