96746b9a96c0e85eaa1cea3f7069d9f2ea07deaf
SB-2803 <siyabaheti2803@gmail.com>
2026-07-24 15:57:43 +0530

    hw

--- Module 3/Factorial.py
+++ Module 3/Factorial.py
@@ -1,7 +1,7 @@
 def factorial(x):
     '''This is a recursive function to find the factorial of an integer.'''
 
-    if x == 0 or x==1:
+    if x == 0 or x == 1:
         return 1
     else:
         #calling function inside a function
--- Module 3/Number guess.py
+++ Module 3/Number guess.py
@@ -2,13 +2,18 @@ import random #importing module
 playing = True
 num = str(random.randint(10,20))
 
-print("A number between 10 to 20 will be generated and you have to guess the number.")
+print("A number between 10 to 20 will be generated and you have to guess the number in 5 chances.")
 print("The game ends when you get the correct guess!!")
+i = 6
+  
 while playing:
     guess = input("Give me your best guess!! \n")
-    if num == guess:
-        print("You win the game!!")
-        print("The number was",num)
-        break
-    else:
-        print("You guess isn't quite right. Try agin!!")
+    for i in range(1,6):
+         i = i-1
+         if num == guess:
+             print("You win the game!!")
+             print("The number was",num)
+             break
+         else:
+              print("You guess isn't quite right. Try agin!!")
+              print("You have",i, "no. of chances left")
